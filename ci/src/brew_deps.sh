8e239938c44fd8289832e871ee5468b2af540766
Seth Hillbrand <seth@kipro-pcb.com>
2025-04-22 13:41:49 -0700

    Add libepoxy ci dep

--- ci/src/brew_deps.sh
+++ ci/src/brew_deps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export BREW_DEPS=(glew bison opencascade glm boost harfbuzz cairo doxygen gettext wget libgit2 libtool autoconf automake cmake swig openssl unixodbc ninja protobuf nng zstd libomp)
+export BREW_DEPS=(glew bison opencascade glm boost harfbuzz cairo doxygen gettext wget libepoxy libgit2 libtool autoconf automake cmake swig openssl unixodbc ninja protobuf nng zstd libomp)
